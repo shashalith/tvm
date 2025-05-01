faa2dbf2b51cc27fb64c6fba139788d7f225e849
rohit21oc <rohitkumar15031@gmail.com>
2025-05-01 17:19:10 +0530

    first commit

---
+++ onboarding-form/src/app/components/previous-employment/previous-employment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-previous-employment',
+  templateUrl: './previous-employment.component.html',
+  styleUrls: ['./previous-employment.component.css']
+})
+export class PreviousEmploymentComponent {
+
+}
