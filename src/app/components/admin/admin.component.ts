de6d09c56c61e222a8e2bf5c757aa7c57ea3770b
ChandruSakthi <chandrus2805@gmail.com>
2025-05-05 18:53:35 +0530

    Firest Change

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,3 +1,41 @@
+// import { Component, OnInit } from '@angular/core';
+// import { EmployeeService } from '../../services/employee.service';
+
+// @Component({
+//   selector: 'app-admin',
+//   templateUrl: './admin.component.html',
+//   styleUrls: ['./admin.component.css']
+// })
+// export class AdminComponent implements OnInit {
+//   employees: any[] = [];
+//   selectedEmployee: any = null;
+
+//   constructor(private empService: EmployeeService) {}
+
+//   ngOnInit(): void {
+//     this.empService.getEmployees().subscribe({
+//       next: (res) => {
+//         this.employees = res.map((data, index) => ({
+//           index: index + 1,
+//           name: data["personal"].fname + ' ' + data["personal"].lname,
+//           email: data["personal"].email,
+//           contact: data["personal"].current_contact,
+//           gender: data["personal"].gender,
+//           details: data
+//         }));
+//       },
+//       error: (err) => {
+//         console.error("Error fetching employees", err);
+//       }
+//     });
+//   }
+
+//   viewDetails(employee: any) {
+//     this.selectedEmployee = employee;
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
 
@@ -8,29 +46,73 @@ import { EmployeeService } from '../../services/employee.service';
 })
 export class AdminComponent implements OnInit {
   employees: any[] = [];
+  filteredEmployees: any[] = [];
   selectedEmployee: any = null;
+  searchText: string = '';
 
   constructor(private empService: EmployeeService) {}
 
+  // ngOnInit(): void {
+  //   this.empService.getEmployees().subscribe({
+  //     next: (res) => {
+  //       this.employees = res.map((data, index) => ({
+  //         index: index + 1,
+  //         name: data.fname + ' ' + data.lname,
+  //         email: data.email,
+  //         contact: data.current_contact,
+  //         gender: data.gender,
+  //         details: data
+  //       }));
+  //       this.filteredEmployees = [...this.employees];
+  //     },
+  //     error: (err) => {
+  //       console.error("Error fetching employees", err);
+  //     }
+  //   });
+  // }
   ngOnInit(): void {
+    // alert("sss")
+    // debugger
     this.empService.getEmployees().subscribe({
-      next: (res) => {
-        this.employees = res.map((data, index) => ({
+      
+      next: (d:any) => {
+       let a= d.body[0]
+        console.log("Response:", a);
+        // console.log(res);
+        
+        // const employeeList = Array.isArray(res) ? res : [res[0]];
+        // console.log(employeeList);
+        
+        this.employees = d.body.map((data: any, index: number) => ({
           index: index + 1,
-          name: data["personal"].fname + ' ' + data["personal"].lname,
-          email: data["personal"].email,
-          contact: data["personal"].current_contact,
-          gender: data["personal"].gender,
+          name: data.fname + ' ' + data.lname,
+          email: data.email,
+          contact: data.current_contact,
+          gender: data.gender,
           details: data
         }));
+        
+  
+        this.filteredEmployees = [...this.employees];
       },
       error: (err) => {
         console.error("Error fetching employees", err);
       }
     });
   }
+  
+  onSearch() {
+    const term = this.searchText.trim().toLowerCase();
+    this.filteredEmployees = this.employees.filter(emp =>
+      emp.name.toLowerCase().includes(term)
+    );
+  }
 
   viewDetails(employee: any) {
     this.selectedEmployee = employee;
   }
+
+  closePopup() {
+    this.selectedEmployee = null;
+  }
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,3 +1,20 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class EmployeeService {
+//   private apiUrl = 'http://localhost:8080/personal/findAll';
+
+//   constructor(private http: HttpClient) {}
+
+//   getEmployees(): Observable<any[]> {
+//     return this.http.get<any[]>(this.apiUrl);
+//   }
+// }
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -6,11 +23,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiUrl = 'http://localhost:8080/employees';
+ 
+  // Directly set the API URL here
+  private apiUrl = 'http://localhost:8080/personal/findAll';
 
   constructor(private http: HttpClient) {}
 
   getEmployees(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
+
+//   Optional: Future search by name feature
+//   searchEmployees(name: string): Observable<any[]> {
+//     return this.http.get<any[]>(${this.apiUrl}?name=${name});
+//   }
 }
